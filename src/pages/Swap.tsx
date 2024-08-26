65f023e079a1ddac81d02946b8c7fd7484e15901
Nityam573 <tripathinityam89@gmail.com>
2024-08-26 13:39:35 +0530

    update

--- src/pages/Swap.tsx
+++ src/pages/Swap.tsx
@@ -4,6 +4,8 @@ import { PanoraWidget, PanoraWidgetConfig } from '@panoraexchange/widget-sdk';
 const PANORA_WIDGET_API_KEY = "oLujOsvnXgFY9TjN5VxS@u@kmq+wWjcyTEnVL4LEPf5pwNtYdR90EfeBDj33F^4E";
 const KEYLESS_GOOGLE_CLIENT_ID = "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";
 
+// Setting up Panora
+
 const widgetConfig: PanoraWidgetConfig = {
   API_KEY: "PANORA_WIDGET_API_KEY",
 }
